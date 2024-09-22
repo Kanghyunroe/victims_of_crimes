96674cbb460ace9db784d05fb5b7b18103e5e680
Kevin Roe <kevinroe0124@gmail.com>
2024-09-22 02:49:00 -0400

    Updated simulate_data for errors in simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -20,49 +20,51 @@ set.seed(369)
 data <- 
   tibble(
     # Use 1 through to 1244 to represent unique IDs
-    "id" = 1:1244,
+    id = 1:1244,
     
     # Randomly assign years
-    "report_year" = sample(2014:2023, size = 1244, replace = TRUE),
+    report_year = sample(2014:2023, size = 1244, replace = TRUE),
     
     # Set category to "Crimes Against the Person" for all rows
-    "category" = "Crimes Against the Person",
+    category = "Crimes Against the Person",
     
-    # Randomly pick subtypes
-    "subtype" = sample(c("Assault", "Theft", "Burglary", "Other"), size = 1244, 
+    # Randomly pick subtype
+    subtype = sample(c("Assault", "Theft", "Burglary", "Other"), size = 1244, 
                        replace = TRUE),
     
     # Randomly assign assault_subtype and N/A to other crimes
-    "assault_subtype" = case_when("subtype" =="Assault" ~
-                                    sample(c("Assault Peace Officer", 
-                                             "Assault Resist Arrest", 
-                                             "Assault Peace Officer Weapon/Bodily Harm", 
-                                             "Aggravated Peace Officer"), 
-                                           size = 1244, replace = TRUE), 
-                                  TRUE ~ "N/A"),
-    
+    assault_subtype = ifelse(
+      subtype == "Assault", 
+      sample( c("Assault Peace Officer", 
+                "Assault Resist Arrest", 
+                "Assault Peace Officer Weapon/Bodily Harm", 
+                "Aggravated Peace Officer"),
+              size = sum(subtype == "Assault"), 
+              replace = TRUE),"N/A"),
+      
     # Randomly assign sex 
-    "sex" = sample(c("M", "F", "U"), size = 1244, replace = TRUE), 
+    sex = sample(c("M", "F", "U"), size = 1244, replace = TRUE), 
     
     # Randomly assign age_group
-    "age_group" = sample(c("Adult", "Unknown", "Child", "Youth"), size = 1244, 
+    age_group = sample(c("Adult", "Unknown", "Child", "Youth"), size = 1244, 
                          replace = TRUE),
     
     # Assign age_cohort based on age_group
-    "age_cohort" = case_when(
-      "age_group" == "Child"   ~ "<12",
-      "age_group" == "Unknown" ~ "Unknown",
-      "age_group" == "Youth"   ~ "12 to 17",
-      "age_group" == "Adult"   ~ sample(c("18 to 24", "25 to 34", 
-                                          "35 to 44", "45 to 54", 
-                                          "55 to 64", "65+"), 
-                                        size = 1244, replace = TRUE)
+    age_cohort = ifelse(age_group == "Child", "<12",
+                        ifelse(age_group == "Unknown", "Unknown",
+                               ifelse(age_group == "Youth", "12 to 17",
+                                      sample(c("18 to 24", "25 to 34", 
+                                               "35 to 44", "45 to 54", 
+                                               "55 to 64", "65+"), 
+                                             size = sum(age_group == "Adult"), 
+                                             replace = TRUE)[1:sum(age_group == 
+                                                                     "Adult")]
+                               )
+                        )
     ),
-  
-    
-  
+
     # Randomly generate count values (based on reasonable estimates from data)
-    "count" = sample(1:500, size = 1244, replace = TRUE)
+    count = sample(1:500, size = 1244, replace = TRUE),
   )
 
 
